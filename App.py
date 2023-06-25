8a3d57b17097ac8097b78d49e5abf620d4f24776
aaarif796 <48153674+aaarif796@users.noreply.github.com>
2023-06-25 16:48:10 +0530

    Update App.py

--- App.py
+++ App.py
@@ -96,6 +96,7 @@ if button:
           model1 = pickle.load(file)
      
      predict1 = model1.predict(a)
+     predict1=str(predict1[0])
      st.write('The prediction of Selling Price is', predict1)
 
 
